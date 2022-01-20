1889a9f8b4b0c192bedc4e980382ce092e84725f
Bazibro <96934642+hyko-xd@users.noreply.github.com>
2022-01-21 01:29:56 +0530

    Update config.py

--- Noinoi/config.py
+++ Noinoi/config.py
@@ -6,7 +6,9 @@ from Noinoi.DREAMS.uptools import fetch_heroku_git_url
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 
+    
 load_dotenv()
+get_queue = {}
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -25,12 +27,15 @@ GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CFC_BOT_SUPPORT")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "BAZIGARXD")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/88b954d019f1879737575.jpg")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Baziibro/NoinoiMusicBot")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 if str(getenv("LOG_SESSION")).strip() == "":
     LOG_SESSION = str(None)
