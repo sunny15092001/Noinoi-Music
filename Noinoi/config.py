83dc4134e5cd8c2d95a208c9bb401db3aa8ac821
Bazibro <96934642+hyko-xd@users.noreply.github.com>
2022-01-21 21:03:58 +0530

    Update config.py

--- Noinoi/config.py
+++ Noinoi/config.py
@@ -1,7 +1,6 @@
 import os
 from os import getenv
 from dotenv import load_dotenv
-from Noinoi.DREAMS.uptools import fetch_heroku_git_url
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
@@ -10,21 +9,21 @@ load_dotenv()
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "ɴᴏɪ ɴᴏɪ ᴍᴜsɪᴄ 🌸")
+BOT_NAME = getenv("BOT_NAME", "Video Stream")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-IMG_1 = getenv("IMG_1", "https://telegra.ph/file/88b954d019f1879737575.jpg")
-IMG_2 = getenv("IMG_2", "https://telegra.ph/file/88b954d019f1879737575.jpg")
-IMG_3 = getenv("IMG_3", "https://telegra.ph/file/88b954d019f1879737575.jpg")
-IMG_4 = getenv("IMG_4", "https://telegra.ph/file/88b954d019f1879737575.jpg")
-OWNER_NAME = getenv("OWNER_NAME", "BAZIGARYT")
-ALIVE_NAME = getenv("ALIVE_NAME", "ᴄғᴄ ᴍᴜsɪᴄ 🔥")
-BOT_USERNAME = getenv("BOT_USERNAME", "NOINOI_BOT")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "NOINOIASSISTANT")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CFC_BOT_SUPPORT")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "BAZIGARXD")
+OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
+ALIVE_NAME = getenv("ALIVE_NAME", "Levina")
+BOT_USERNAME = getenv("BOT_USERNAME", "veezvideobot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "cleo_invida")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/88b954d019f1879737575.jpg")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Baziibro/NoinoiMusicBot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-stream")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
