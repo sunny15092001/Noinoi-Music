4913584cb99c973a476c0ff9fa7b02a2dc00467d
Bazibro <96934642+hyko-xd@users.noreply.github.com>
2022-01-22 13:13:09 +0530

    Update music.py

--- Noinoi/PLUGINS/music.py
+++ Noinoi/PLUGINS/music.py
@@ -1,16 +1,14 @@
-# Copyright (C) 2021 By Veez Music-Project
-# Commit Start Date 20/10/2021
-# Finished On 28/10/2021
+# noinoi music
 
 import re
 import asyncio
 
-from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
-from driver.design.thumbnail import thumb
-from driver.design.chatname import CHAT_TITLE
-from driver.filters import command, other_filters
-from driver.queues import QUEUE, add_to_queue
-from driver.veez import call_py, user
+from Noinoi.config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
+from Noinoi.OTHERS.design.thumbnail import thumb
+from Noinoi.OTHERS.design.chatname import CHAT_TITLE
+from Noinoi.DREAMS.filters import command, other_filters
+from Noinoi.DREAMS.queues import QUEUE, add_to_queue
+from Noinoi.DREAMS.cfc import call_py, user
 from driver.utils import bash
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
