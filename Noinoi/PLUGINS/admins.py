4d79aacc1b36cd33f5b480f58404f0ef90fc4e91
Bazibro <96934642+hyko-xd@users.noreply.github.com>
2022-01-22 13:17:03 +0530

    Update admins.py

--- Noinoi/PLUGINS/admins.py
+++ Noinoi/PLUGINS/admins.py
@@ -1,11 +1,11 @@
 from cache.admins import admins
-from driver.veez import call_py
+from Noinoi.DREAMS.cfc import call_py
 from pyrogram import Client, filters
-from driver.decorators import authorized_users_only
-from driver.filters import command, other_filters
-from driver.queues import QUEUE, clear_queue
-from driver.utils import skip_current_song, skip_item
-from config import BOT_USERNAME, GROUP_SUPPORT, IMG_3, UPDATES_CHANNEL
+from Noinoi.DREAMS.decorators import authorized_users_only
+from Noinoi.DREAMS.filters import command, other_filters
+from Noinoi.DREAMS.queues import QUEUE, clear_queue
+from Noinoi.DREAMS.utils import skip_current_song, skip_item
+from Noinoi.config import BOT_USERNAME, GROUP_SUPPORT, IMG_3, UPDATES_CHANNEL
 from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
